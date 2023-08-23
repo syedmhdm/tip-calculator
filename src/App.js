6b1740217a582a3bf732c97ba70cdfec94cc1d22
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-08-23 20:40:48 +0530

    tip calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [bill, setBill] = useState(0);
+  const [myTip, setMyTip] = useState(0);
+  const [friendTip, setfriendTip] = useState(0);
+
+  const tipPercentage = (myTip + friendTip) / 2;
+  const tip = ((bill / 100) * tipPercentage).toFixed(2);
+  const totalPay = Number(bill) + Number(tip);
+
+  function reset() {
+    setBill(0);
+    setMyTip(0);
+    setfriendTip(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BillInput bill={bill} onBillChange={setBill} />
+      <SelectPercentage
+        tip={myTip}
+        onSetTip={setMyTip}
+        text={"How did you like the service?"}
+      />
+      <SelectPercentage
+        tip={friendTip}
+        onSetTip={setfriendTip}
+        text={"How did your friend like the service?"}
+      />
+      <Output totalPay={totalPay} bill={bill} tip={tip} />
+      <Reset reset={reset} />
     </div>
   );
 }
 
 export default App;
+
+function BillInput({ bill, onBillChange }) {
+  return (
+    <div>
+      <p>How much was the bill</p>
+      <input
+        type='number'
+        value={bill}
+        onChange={(e) =>
+          onBillChange(e.target.value === "" ? "" : Number(e.target.value))
+        }
+      />
+    </div>
+  );
+}
+
+function SelectPercentage({ text, onSetTip, tip }) {
+  return (
+    <div>
+      <p>{text}</p>
+      <select value={tip} onChange={(e) => onSetTip(Number(e.target.value))}>
+        <option value={0}>Dissatisfied (0%)</option>
+        <option value={5}>It was okay (5%)</option>
+        <option value={10}>It was good (10%)</option>
+        <option value={20}>Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+function Output({ totalPay, bill, tip }) {
+  if (bill === 0 || bill === "")
+    return <h3>Enter the bill amount to calculate</h3>;
+
+  return (
+    <h3>
+      You pay {totalPay} (${bill} + ${tip} tip)
+    </h3>
+  );
+}
+function Reset({ reset }) {
+  return <button onClick={reset}>Reset</button>;
+}
